55f859df6a9c25943993bb4aad4c4ba5d50d3436
NINK-cyber <nink190360@gmail.com>
2026-04-08 02:46:08 +0000

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,14 +1,13 @@
 import sys
 import os
 import logging
-from flask import Flask, request
+from flask import Flask
 import requests
 import argparse
 import colorama
 from colorama import Fore, Style
 
-# SILENCIADOR DO FLASK
-os.environ['WERKZEUG_RUN_MAIN'] = 'true'
+
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
@@ -30,51 +29,50 @@ def proxy(path):
         resposta = requests.get(f"{alvo}/{path}", timeout=3)
         return (resposta.content, resposta.status_code, list(resposta.headers.items()))
     except:
-        return f"Erro no nó {indice + 1}", 502
+        return "Erro no nó", 502
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=8080)
     args = parser.parse_args()
 
-    # O AVIÃO CORRIGIDO (SEM ESPAÇOS SOBRANDO)
+    
     banner = r"""
-              _   _
-             | | | |
-  __ _ _ __   __| | | |
- / _` | '_ \ / _` | | |
+          _   _
+         | | | |
+  __ _ _ __  __| | | |
+ / _` | '_ \/ _` | | |
 | (_| | | | | (_| | |_|
  \__,_|_| |_|\__,_| (_)
 
-             .o+`
-            `ooo/
-           `+oooo:
-          `+oooooo:
-         -+oooooo+:
-        `/: -:++oooo+:
-       `/++++/+++++++:
-      `/+++++++++++++++:
-     `/+++oooooooooooooo/`
-    ./ooossssso++ossssssso+`
-   .oossssso-````/ossssss+`
-   -ossssso.      :ssssssso.
-   :osssssss/      osssso+++.
-   /osssssss/      +ssssooo/-
-   `/ossssso+/: -   -: /+osssso+-
-    `+sso+:-`          `.-/+oso:
-     `++: .                   `-/+
-      `.
+          .o+`
+         `ooo/
+        `+oooo:
+       `+oooooo:
+      -+oooooo+:
+     `/: -:++oooo+:
+    `/++++/+++++++:
+   `/+++++++++++++++:
+  `/+++oooooooooooooo/`
+ ./ooossssso++ossssssso+`
+.oossssso-````/ossssss+`
+-ossssso.      :ssssssso.
+:osssssss/      osssso+++.
+/osssssss/      +ssssooo/-
+`/ossssso+/: -   -: /+osssso+-
+ `+sso+:-`          `.-/+oso:
+  `++: .                   `-/+
+   `.
     """
     
-    # LIMPA A TELA ANTES DE MOSTRAR
-    os.system('cls' if os.name == 'nt' else 'clear')
-    
+    os.system('clear')
     print(Fore.LIGHTRED_EX + banner)
-    print(Fore.GREEN + f" [+] GhostHop v2.3 | {len(SERVIDORES)} nós ativos")
+    print(Fore.GREEN + f" [+] GhostHop v2.4 | {len(SERVIDORES)} nós ativos")
     print(Fore.RED + " [+] Dev: FOREVER")
-    print(Fore.WHITE + " " + "-"*45)
+    print("-" * 45)
     print(Fore.CYAN + f" [*] Escutando na porta: {args.port}")
 
+   
     app.run(port=args.port, threaded=True, debug=False, use_reloader=False)
 
 if __name__ == '__main__':
